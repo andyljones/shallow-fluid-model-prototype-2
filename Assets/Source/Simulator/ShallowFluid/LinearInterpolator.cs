4d7978753f348504dac819466d3b0335b46c1694
Andy Jones <andyjones.ed@gmail.com>
2013-11-29 15:33:07 +0000

    Added Iridium; about to add ScalarFieldMap syntactic sugar.

---
+++ Assets/Source/Simulator/ShallowFluid/LinearInterpolator.cs
@@ -0,0 +1,10 @@
+﻿namespace Simulator.ShallowFluid
+{
+    public class LinearInterpolator<T> : IInterpolator<T>
+    {
+        public ScalarField<T> Interpolate(ScalarField<T> field)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Source/Simulator/ShallowFluid/ScalarFieldMap.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Simulator.ShallowFluid
+{
+    /// <summary>
+    /// Syntactic sugar for a dictionary of scalar fields.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ScalarFieldMap<T> : Dictionary<T, ScalarField<T>> 
+    {
+    }
+}
